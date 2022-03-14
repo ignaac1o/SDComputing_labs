7891e8acde7e6ff5e4db06ce8ff1e6e1519028ff
javimuflo16 <javimuflo16@gmail.com>
2022-03-14 11:29:50 +0100

    last changes to serial version

--- k_means_serial.py
+++ k_means_serial.py
@@ -1,105 +1,108 @@
 
-# Import libraries
+##############################################################################################################################################
+######################################################  PART 1: SERIAL VERSION. OPTION A  ####################################################
+##############################################################################################################################################
+
+# Import libraries 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import time
+import multiprocessing as mp
 
-# Function to standarize dataset
-def standarize_data(data_input):
-    return data_input.apply(lambda x: (x-x.mean())/ x.std())
-
-# Function to initialize centroids randomly
-def initialize_centroids(k, data):
-    """
-    This function initialize centroids randomly. In particular, this function choose an observation at random 
-    and assigns it as a centroid.
-    k: number of clusters.
-    data: dataset.
-    """
-    return data.sample(n=k).reset_index(drop=True)
-
-def dist_eucl(a,b):
-  return np.sqrt(np.sum((a-b)**2))
-
-def assign_obs(data, centroids):
-    """
-    It calculates the distance between centroids and each observation of the dataset. Finally, it assigns each
-    observation to the nearest cluster. It returns two lists: a list which contains the assignments and other list 
-    with the distance associated. 
-    data: dataset.
-    centroids: the observations which work as centroids.
-    """
-
-    n , k = (data.shape[0], centroids.shape[0])
-
-    distances = pd.DataFrame()
-    for i in range(k): 
-      distances.loc[:,i] = list(data.apply(dist_eucl, args = [centroids.iloc[i,:]], axis=1))
-      min_v , min_index = list(distances.min(axis = 1)) , list(distances.idxmin(axis = 1))
-
-    return (min_index, min_v)
-
-def cluster_iterations(dataset, k, n_iterations):
+# Implement k-means algorithm
+def k_means(dataset, k):
     '''
-    It computes several iterations relocating centroids to achieve the final distribution of clusters.
-    data: dataset
-    k: number of clusters
+    It computes several iterations relocating centroids to achieve the final distribution of clusters. The algorithm stops when the centroids of
+    te iteration i and the ones of the iteration i-1 match. This will be the way to convergence. The function returns the input dataset adding up 
+    two new variables: the assignment and the errors associated.
+    
+    PARAMETERS:
+    "dataset": dataset
+    "k": number of clusters
     '''
 
     data = dataset.copy()
 
-
-    # Initialize centroids and error
-    centroids = initialize_centroids(k, data)
-    error = []
+    # Initialize centroids and error.
+    n = data.shape[0]
+    new_centroids = data.sample(n=k, random_state=100).reset_index(drop=True) # Select random observations as initial centroids.
+    error = np.array([])
     it = 0
+    n_iter = True
+    old_centroids = np.zeros((k, data.shape[1]))
 
-    for i in range(n_iterations):
-      data['centroid'], data['errors'] = assign_obs(data, centroids)
-      error.append(sum(data['errors']))
-      centroids = data.groupby('centroid').agg('mean').reset_index(drop = True)
+    while(n_iter):
+      errors = np.array([n])
+      distances = pd.DataFrame()
+      for i in range(k): 
+        distances.loc[:,i] = np.array(data.apply(lambda a,b: np.sqrt(np.sum((a-b)**2)), args = [new_centroids.iloc[i,:]], axis=1))
+        min_v , min_index = np.array(distances.min(axis = 1)) , np.array(distances.idxmin(axis = 1))
+      data['centroid'], errors = min_index , min_v
+      error = np.append(error, np.sum(errors))
+      new_centroids = data.groupby('centroid').agg('mean').reset_index(drop = True)
       it += 1
-      if it > 2 and (round(error[i]) == round(error[i-1])):
-          break
+      if (np.round(new_centroids,1) == np.round(old_centroids,1)).all().all():
+        n_iter = False
       else:
-          continue
-
-    data['centroid'], data['errors'] = assign_obs(data,centroids)
+        old_centroids = new_centroids
+        n_iter = True
+      
+    data['errors'] = errors
     return (data)
 
-# Load the dataset
-data_read = pd.read_csv("computers.csv").iloc[:,1:7] # We discard the index (first column) and categorical variables
+# Load the dataset. We discard the index (first column) and categorical variables
+data_read = pd.read_csv("computers5k.csv").drop(['id', 'laptop', 'cd', 'trend'], axis=1) 
 
 # Standarize data
-data_norm = standarize_data(data_read)
+data_norm = data_read.apply(lambda x: (x-x.mean())/ x.std())
 
-#Elbow Graph
-start = time.time()
+#Elbow Graph.
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("------------------------------------------------ ELBOW GRAPH ----------------------------------------------------------------------")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+
 n = 5
-n_it = 5
-squares = [sum((cluster_iterations(data_norm,i+1, n_it))['errors']) for i in range(n)]
+start = time.time()
+squares = [np.sum((k_means(data_norm,i+1))['errors']) for i in range(n)]
 end = time.time()
 print("Time elapsed:",end - start,"seconds")
 plt.figure(figsize=(10,5))
 plt.title("Elbow Graph")
+plt.xlabel("Number of clusters (k) ", size = 16,)
+plt.ylabel("WSS", size = 16)
 plt.plot(range(1,n+1), squares)
 plt.show()
 
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("------------------------------ The optimal value for k is 2. We run the algorith with k = 2 ---------------------------------------")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+
 # k=2, optimal clusters number
 # Run the algorithm with k=2
-# We measure time with time
+# We measure time 
 k_final = 2
-n_it = 4
 start = time.time()
-data_kmeans = cluster_iterations(data_norm, k_final, n_it)
+data_kmeans = k_means(data_norm, k_final)
 end = time.time()
 print("Time elapsed:",end - start,"seconds")
 
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("------------------------------------------------ We plot the first two dimensions -------------------------------------------------")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+
 # Plot the first two dimensions of the clusters
-sns.scatterplot(x="price", y="speed", hue="centroid", data=data_kmeans)
+plt.figure(figsize=(10,5))
+plt.title("First two dimensions")
+plt.xlabel("Price ", size = 16,)
+plt.ylabel("Speed", size = 16)
+sns.scatterplot(x="price", y="speed", hue="centroid", data=data_kmeans.drop("errors", axis = 1))
 plt.show()
+
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("------------------------------------------------ The the cluster with the highest average price -----------------------------------")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
 
 # We see the cluster with largest average price
 mean = []
@@ -107,8 +110,14 @@ for i in range(k_final-1):
   mean.append(np.mean(data_kmeans['price'].loc[(data_kmeans.centroid == i)]))
   print("The cluster with the largest average price is", mean.index(max(mean)), ",with mean", min(mean))
 
-# Print a heat map
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("-------------------------------------------------------- HEAT MAP -----------------------------------------------------------------")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+# Print the heat map
 data_kmeans_we = data_kmeans.drop("errors", axis = 1)
+plt.figure(figsize=(10,5))
+plt.title("Heat map")
+plt.xlabel("Variables", size = 16,)
+plt.ylabel("Centroids", size = 16)
 sns.heatmap(data_kmeans_we.groupby('centroid').agg('mean').reset_index(drop = True))
-plt.show()
-
+plt.show() 
